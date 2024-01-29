4e9b2a6c2b3c931ee50546e20a73c8b09e7bccbf
superJarXukkk <1793726627qq@gmail.com>
2024-01-29 14:04:56 +0800

    5.分类分页查询和员工添加的异常处理模块已添加

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,48 @@
+package com.sky.controller.admin;
+
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Api("分类功能模块")
+@RestController
+@RequestMapping("/admin/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PutMapping("")
+    public Result update(){
+        return Result.success();
+    }
+
+    @GetMapping("/page")
+    public Result<PageResult> getPage(CategoryPageQueryDTO categoryPageQueryDTO){
+        PageResult pageResult = categoryService.getPage(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @PostMapping("/status/{status}")
+    public Result statusOnOrDown(@PathVariable Integer status,Long id){
+        return Result.success();
+    }
+
+    @PostMapping("")
+    public Result add(@RequestBody CategoryDTO categoryDTO){
+        return Result.success();
+    }
+
+    @DeleteMapping("")
+    public Result delete(Long id){
+        return Result.success();
+    }
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,30 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.mapper.CategoryMapper;
+import com.sky.result.PageResult;
+import com.sky.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public PageResult getPage(CategoryPageQueryDTO categoryPageQueryDTO) {
+        PageHelper.startPage(categoryPageQueryDTO.getPage(),categoryPageQueryDTO.getPageSize());
+
+        Page<Category> page = categoryMapper.queryForPage(categoryPageQueryDTO);
+        PageResult pageResult = new PageResult();
+        pageResult.setTotal(page.getTotal());
+        pageResult.setRecords(page.getResult());
+
+        return pageResult;
+    }
+}
